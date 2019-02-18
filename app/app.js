d05a40fe7673d5179a00f1527118bbab1baaf7a5
Kevin Solimo <kevinsolimo@gmail.com>
2019-02-18 16:01:47 +0000

    First Commit

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var server = require('http').createServer(app);
-var io = require('../..')(server);
+var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
 server.listen(port, () => {
@@ -15,8 +15,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Chatroom
 
-var numUsers = 0;
-
 io.on('connection', (socket) => {
   var addedUser = false;
 
@@ -27,6 +25,7 @@ io.on('connection', (socket) => {
       username: socket.username,
       message: data
     });
+    
   });
 
   // when the client emits 'add user', this listens and executes
@@ -35,28 +34,10 @@ io.on('connection', (socket) => {
 
     // we store the username in the socket session for this client
     socket.username = username;
-    ++numUsers;
     addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    });
+    socket.emit('login', {});
     // echo globally (all clients) that a person has connected
     socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
-    });
-  });
-
-  // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', () => {
-    socket.broadcast.emit('typing', {
-      username: socket.username
-    });
-  });
-
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', () => {
-    socket.broadcast.emit('stop typing', {
       username: socket.username
     });
   });
@@ -64,12 +45,10 @@ io.on('connection', (socket) => {
   // when the user disconnects.. perform this
   socket.on('disconnect', () => {
     if (addedUser) {
-      --numUsers;
 
       // echo globally that this client has left
       socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
+        username: socket.username
       });
     }
   });
